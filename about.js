d05261ada31500507e92f6c8442e05065066c31f
Xavier Brown <xavieraustin1990@gmail>
2023-09-29 15:07:25 -0400

    personalizing the website

--- about.js
+++ about.js
@@ -5,10 +5,39 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('form submit');
+}
+
+let mouseHover = document.querySelector('#cutecat');
+let extendCompliment = document.querySelector('#compliment')
+
+mouseHover.addEventListener('hover', extendCompliment);
+
+if (extendCompliment.includes('a'))
+	console.log(extendCompliment)
+else {
+	console.log(extendCompliment)
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+const divs = document.querySelector('div')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
